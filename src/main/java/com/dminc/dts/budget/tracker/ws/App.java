4fddc81b4107a3a53ed5faf0c4643be7836f91dc
Jim Garlick <jgarlick@allegient.com>
2018-02-09 11:06:12 -0500

    Added config object to expose IDs

--- src/main/java/com/dminc/dts/budget/tracker/ws/App.java
+++ src/main/java/com/dminc/dts/budget/tracker/ws/App.java
@@ -1,7 +1,5 @@
 package com.dminc.dts.budget.tracker.ws;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -15,10 +13,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 public class App {
 
-    private static final Logger log = LoggerFactory.getLogger(App.class);
-
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
     }
+
 
 }
